a0f4571b9234eca95a914bd2a33aab04d54d0593
Ryan Lynn Rosas <rltrosas88@gmail.com>
2021-06-16 19:02:14 -0500

    GET and POST method

--- server.js
+++ server.js
@@ -21,9 +21,36 @@ const PORT = process.env.PORT || 3001;
 //app.use(express.json());
 
 app.get('/api/notes', (req, res) => {
-  let results = notes;
-  console.log(req.query)
-  res.json(results);
+  //console.log(req.query)
+  res.json(notes);
+});
+
+function writeToDb(notes) {
+  notes = JSON.stringify(notes);
+  console.log(notes);
+  fs.writeFileSync("./db/db.json", notes, function(err) {
+    if (err) {
+      return console.log(err);
+    }
+  });
+}
+
+app.post('/api/notes', (req, res) => {
+  //req.body.id = notes.length.toString();
+
+  if (notes.length == 0){
+    req.body.id = "0";
+  } else {
+    req.body.id = JSON.stringify(JSONparse(notes[notes.length - 1].id) = 1);
+  }
+  console.log("req.body.id: " + req.body.id);
+
+  notes.push(req.body);
+
+  writeToDb(notes);
+  console.log(notes);
+
+  res.json(req.body);
 });
 
 app.listen(PORT, () => {
