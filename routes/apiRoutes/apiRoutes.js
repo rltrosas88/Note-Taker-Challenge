c7127bd7f30d66b2f0db9350052ffcca2c475cae
Ryan Lynn Rosas <rltrosas88@gmail.com>
2021-06-16 22:08:11 -0500

    added lib folder to move functions to

--- routes/apiRoutes/apiRoutes.js
+++ routes/apiRoutes/apiRoutes.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const { notes } = require('../../db/db.json')
 const router = require('express').Router();
+const { filterByQuery, create, validate, deleteNotes } = require('../../lib/notes');
 
 // function writeToDb(notes) {
 //     notes = JSON.stringify(notes);
@@ -11,42 +12,6 @@ const router = require('express').Router();
 //       }
 //     });
 //   }
-
-function filterByQuery(query,notesArray) {
-  let filterdResults = notesArray;
-  console.log("query", query)
-  if (query) {
-    filteredResults = filteredResults.filter((note) => note.id !== query);
-  }
-  return filterdResults;
-};
-
-function create(body, notesArray) {
-  const note = body;
-  notesArray.push(note);
-  fs.writeFileSync(
-    path.join(__dirname, "../../db/db.json"),
-    JSON.stringify(notesArray),
-  );
-  return note;
-};
-
-function validate(note) {
-  if (!note.title || typeof note.title !== "string") {
-    return false;
-  }
-  if (!note.text || typeof note.text !== "string") {
-    return false;
-  }
-};
-
-function deleteNotes(newNotesArray) {
-  fs.writeFileSync (
-    path.join(__dirname, "../../db/db.json"),
-    JSON.stringify(newNotesArray)
-  );
-  return newNotesArray;
-};
 
 router.get('/api/notes', (req, res) => {
   let results = notes;  
