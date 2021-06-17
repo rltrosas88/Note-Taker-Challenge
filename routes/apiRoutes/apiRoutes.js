19954e1f30ab38a5a3112f05de17a78bceeb43cc
Ryan Lynn Rosas <rltrosas88@gmail.com>
2021-06-16 21:50:22 -0500

    trying different functions

--- routes/apiRoutes/apiRoutes.js
+++ routes/apiRoutes/apiRoutes.js
@@ -2,18 +2,55 @@ const fs = require('fs');
 const { notes } = require('../../db/db.json')
 const router = require('express').Router();
 
-function writeToDb(notes) {
-    notes = JSON.stringify(notes);
-    console.log(notes);
-    fs.writeFileSync("../../db/db.json", notes, function(err) {
-      if (err) {
-        return console.log(err);
-      }
-    });
+// function writeToDb(notes) {
+//     notes = JSON.stringify(notes);
+//     console.log(notes);
+//     fs.writeFileSync("../../db/db.json", notes, function(err) {
+//       if (err) {
+//         return console.log(err);
+//       }
+//     });
+//   }
+
+function filterByQuery(query,notesArray) {
+  let filterdResults = notesArray;
+  console.log("query", query)
+  if (query) {
+    filteredResults = filteredResults.filter((note) => note.id !== query);
   }
+  return filterdResults;
+};
+
+function create(body, notesArray) {
+  const note = body;
+  notesArray.push(note);
+  fs.writeFileSync(
+    path.join(__dirname, "../../db/db.json"),
+    JSON.stringify(notesArray),
+  );
+  return note;
+};
+
+function validate(note) {
+  if (!note.title || typeof note.title !== "string") {
+    return false;
+  }
+  if (!note.text || typeof note.text !== "string") {
+    return false;
+  }
+};
+
+function delete(newNotesArray) {
+  fs.writeFileSync (
+    path.join(__dirname, "../../db/db.json"),
+    JSON.stringify(newNotesArray)
+  );
+  return newNotesArray;
+};
 
 router.get('/api/notes', (req, res) => {
-    //console.log(req.query);
+  let results = notes;  
+  //console.log(req.query);
     res.json(notes);
 });
 
@@ -34,5 +71,6 @@ router.post('/api/notes', (req, res) => {
 
     res.json(req.body);
   });
+
 
   module.exports = router;
